0cf48912ada6d4d11a2b2eed03ffadee546ef845
codeHysteria28 <graphicglobal67@gmail.com>
2022-05-12 14:33:55 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,41 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup("resourceGroup");
+
+// Create an Azure resource (Storage Account)
+const storageAccount = new storage.StorageAccount("sb", {
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        name: storage.SkuName.Standard_LRS,
+    },
+    kind: storage.Kind.StorageV2,
+});
+
+// Export the primary key of the Storage Account
+const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
+    storage.listStorageAccountKeys({ resourceGroupName, accountName }));
+export const primaryStorageKey = storageAccountKeys.keys[0].value;
+
+// Enable static website support
+const staticWebsite = new storage.StorageAccountStaticWebsite("staticWebsite0011", {
+    accountName: storageAccount.name,
+    resourceGroupName: resourceGroup.name,
+    indexDocument: "index.html",
+});
+
+// Upload the index file
+const indexHtml = new storage.Blob('index.html', {
+    resourceGroupName: resourceGroup.name,
+    accountName: storageAccount.name,
+    containerName: staticWebsite.containerName,
+    source: new pulumi.asset.FileAsset("index.html"),
+    contentType: "text/html",
+});
+
+// Web endpoint to the website
+export const staticEndpoint = storageAccount.primaryEndpoints.web;
+
+
